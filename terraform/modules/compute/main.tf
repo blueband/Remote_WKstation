caf1d193812599c0e97061dd1192d3f12cae3863
Qamarudeen Muhammad <crescentgroup2000@gmail.com>
2022-05-08 15:53:47 +0100

    zone argument disabled

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "default" {
-  name           = var.machine_name
-  machine_type   = var.wkstation_machine_type
-  zone           = var.compute_zone
+  name         = var.machine_name
+  machine_type = var.wkstation_machine_type
+  # zone           = var.compute_zone
   desired_status = var.desired_status
 
 
@@ -10,10 +10,6 @@ resource "google_compute_instance" "default" {
       image = var.os_image
       type  = var.disk_type
     }
-  }
-
-  metadata = {
-    monitor_uptime_script = "https://gist.githubusercontent.com/justinshenk/312b5e0ab7acc3b116f7bf3b6d888fa4/raw/59f021c2bf0388ba36e5a589dba52e233ee84964/idle-shutdown.sh"
   }
 
   network_interface {
