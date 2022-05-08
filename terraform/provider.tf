f4918e2231cf149cbd0d05304c1f32f81fc7ed69
Qamarudeen Muhammad <crescentgroup2000@gmail.com>
2022-05-08 10:01:20 +0100

    backend change to GCS

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,20 +1,20 @@
 # The configuration for the `remote` backend.
 terraform {
   # Terraform Backend  
-  backend "remote" {
-    #         # The name of your Terraform Cloud organization.
-    organization = "bluemoonltd"
-    #
-    #         # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-      name = "gh-action-setup"
-    }
-  }
-
-  #   backend "gcs" {
-  #   bucket  = "tf-tfstatefile"
-  #   prefix  = "terraform/state"
+  # backend "remote" {
+  #   #         # The name of your Terraform Cloud organization.
+  #   organization = "bluemoonltd"
+  #   #
+  #   #         # The name of the Terraform Cloud workspace to store Terraform state files in.
+  #   workspaces {
+  #     name = "gh-action-setup"
+  #   }
   # }
+# GOOGLE Backend
+    backend "gcs" {
+    bucket  = "tf-tfstatefile"
+    prefix  = "terraform/state"
+  }
 }
 
 provider "google" {
